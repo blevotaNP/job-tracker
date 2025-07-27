2354efc5ddc0db42642dbadc3ad3345051b3e782
MykytaProkhoda <prokhodanikita@gmail.com>
2025-07-27 23:40:35 +0200

    manually created first user for testing

--- src/main/java/com/example/jobtracker/Config/SecurityConfig.java
+++ src/main/java/com/example/jobtracker/Config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -43,6 +45,12 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService(){
+        UserDetails user1 = User
+                .withDefaultPasswordEncoder()
+                .username("qwe")
+                .password("123")
+                .roles("user")
+                .build();
         return new InMemoryUserDetailsManager();
     }
 
